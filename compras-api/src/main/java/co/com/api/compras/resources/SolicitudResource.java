3e864863fbd664228d768842e2540fc81f94a055
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2023-06-07 23:25:12 -0500

    Resource configured, a proper status code to be lauched still pending

--- compras-api/src/main/java/co/com/api/compras/resources/SolicitudResource.java
+++ compras-api/src/main/java/co/com/api/compras/resources/SolicitudResource.java
@@ -1,4 +1,50 @@
 package co.com.api.compras.resources;
 
+import co.com.api.compras.dto.SolicitudDTO;
+import co.com.api.compras.service.SolicitudService;
+import co.com.api.compras.utils.Usuario;
+import io.smallrye.mutiny.Uni;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import lombok.AllArgsConstructor;
+
+@Path("/compras")
+@Produces(MediaType.APPLICATION_JSON)
+@AllArgsConstructor
 public class SolicitudResource {
+    private final SolicitudService service;
+
+    @GET
+    public Uni<Response> obtenerTodo(){
+        return service.listAll().onItem().transform(solicitudDTOS -> Response.ok(solicitudDTOS).build());
+    }
+
+    @GET
+    @Path("/{idSolicitud}")
+    public Uni<Response> obtenerPorId(@PathParam("idSolicitud") String idSolicitud){
+        return service.listById(idSolicitud).onItem().transform(solicitudDTO -> Response.ok(solicitudDTO).build());
+    }
+
+    @GET
+    @Path("/usuario")
+    public Uni<Response> obtenerPorUsuario(Usuario usuario){
+        return  service.listByUsuario(usuario).toUni().onItem().transform(solicitudDTO -> Response.ok(solicitudDTO).build());
+    }
+
+    @POST
+    public Uni<Response> guardar(SolicitudDTO solicitudDTO){
+        return service.save(solicitudDTO).onItem().transform(solicitudDTO1 -> Response.status(201).entity(solicitudDTO1).build());
+    }
+
+    @PUT
+    public Uni<Response> editar(SolicitudDTO solicitudDTO){
+        return service.update(solicitudDTO).onItem().transform(solicitudDTO1 -> Response.status(202).entity(solicitudDTO1).build());
+    }
+
+    @DELETE
+    @Path("/{idSolicitud}")
+    public Uni<Response> eliminar(@PathParam("idSolicitud") String idSolicitud){
+        return service.delete(idSolicitud).onItem().transform(aLong -> Response.ok(aLong).build());
+    }
 }
