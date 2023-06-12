a76429ada619404c77546c641f71cd09795212d5
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2023-06-11 23:29:35 -0500

    Reorganization of client package

---
+++ pagos-api/src/main/java/co/com/api/pagos/client/SolicitudService.java
@@ -0,0 +1,19 @@
+package co.com.api.pagos.client;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.annotation.ClientHeaderParam;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@Path("/compras")
+@RegisterRestClient(configKey = "compras-api")
+@Produces(MediaType.APPLICATION_JSON)
+@ClientHeaderParam(name = "caller-from-pagos",value = "compras-api")
+public interface SolicitudService {
+
+    @GET
+    @Path("/{idSolicitud}")
+    SolicitudDTO listById(@PathParam("idSolicitud") String idSolicitud);
+
+    @PUT
+    SolicitudDTO update(SolicitudDTO solicitudDTO);
+}
